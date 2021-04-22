b198eab0fc1c5ed7fd194d775b9ccc8e6c54a5db
Arturo Hernández <arturohrdez@gmail.com>
2021-04-22 14:40:09 -0500

    Send Notification Email

--- index.php
+++ index.php
@@ -1,15 +1,29 @@
 <?php 
-ini_set('display_errors', 0);
+ini_set('display_errors', 1);
 error_reporting(E_ERROR);
 $instancia = $_GET["instancia"];
 require_once "libs/vendor/autoload.php";
+require "PHPMailer/PHPMailer.php";
+require "PHPMailer/SMTP.php";
+require "PHPMailer/Exception.php";
 use PhpOffice\PhpSpreadsheet\IOFactory;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
+//Configuración SMTP
+$config["smpt_secure"]         = "ssl";
+$config["smpt_server"]         = "mail.vicom.mx"; // sets the SMTP server
+$config["smpt_port_conection"] = 465;// set the SMTP port for the GMAIL server
+$config["smpt_user"]           = "no-reply@vicom.mx"; // SMTP account username
+$config["smpt_pwd"]            = "OC8wV^HDRs!G";// SMTP account password
+$config["smpt_name_from"]      = "Vicom";
+$config["subject_title"]       = "Vicom | Axo Files Validate";
+$config["to"]                  = "arturohrdez@gmail.com,arturohrdez@outlook.com";
 
 
 if(empty($instancia)){
-    echo "<h3>La instancia es requerida</h3>";
-    die();
+    die("<h3>La instancia es requerida</h3>");
 }else{
     //Busca archivo para procesar
     $dir = getcwd()."/".$instancia;
@@ -19,6 +33,9 @@ if(empty($instancia)){
 
     //Valida si existe archivos con extensión "xlsx" y "xls"
     $response_file_valid = [];
+    $decode_error        = "";
+    $path_files          = [];
+
     if(!empty($files_xlsx) || !empty($files_xls)){
         if(!empty($files_xlsx)){
             foreach ($files_xlsx as $file_item_xlsx) {
@@ -33,8 +50,6 @@ if(empty($instancia)){
             }
         }//end if
 
-
-
         //Muestra errores en archivos procesados "xlsx"
         if(!empty($response_file_valid_xlsx)){
             foreach ($response_file_valid_xlsx as $res_item_xlsx) {
@@ -45,19 +60,29 @@ if(empty($instancia)){
                 //No hubo error el archivo es válido
                 if($err == 0){
                     rename($dir."/".$file_name, $dir."/procesados/".$file_name);
-                    echo decodeError($err,$arr_error,$file_name);
+                    $decode_error .= decodeError($err,$arr_error,$file_name);
+                    $path_files[] = $dir."/procesados/".$file_name;
                 }//end if
 
                 //Error 1 [Faltán columnas obligatorias]
                 if($err == 1){
                     rename($dir."/".$file_name, $dir."/erroneos/".$file_name);
-                    echo decodeError($err,$arr_error,$file_name);
+                    $decode_error .= decodeError($err,$arr_error,$file_name);
+                    $path_files[] = $dir."/erroneos/".$file_name;
                 }//end if
 
+                //Error 2 [Falta información en requerida en el contenido del excel]
                 if($err == 2){
                     rename($dir."/".$file_name, $dir."/erroneos/".$file_name);
-                    echo decodeError($err,$arr_error,$file_name);
+                    $decode_error .= decodeError($err,$arr_error,$file_name);
+                    $path_files[] = $dir."/erroneos/".$file_name;
                 }//end if
+
+                //Error 404 [No se encontro el archivo]
+                if($err == 404){
+                    $decode_error .= decodeError($err,$arr_error,$file_name);
+                }//end if
+
             }//end foreach
         }//end if
 
@@ -71,27 +96,36 @@ if(empty($instancia)){
                 //No hubo error el archivo es válido
                 if($err == 0){
                     rename($dir."/".$file_name, $dir."/procesados/".$file_name);
-                    echo decodeError($err,$arr_error,$file_name);
+                    $decode_error .= decodeError($err,$arr_error,$file_name);
+                    $path_files[] = $dir."/procesados/".$file_name;
                 }//end if
 
                 //Error 1 [Faltán columnas obligatorias]
                 if($err == 1){
                     rename($dir."/".$file_name, $dir."/erroneos/".$file_name);
-                    echo decodeError($err,$arr_error,$file_name);
+                    $decode_error .= decodeError($err,$arr_error,$file_name);
+                    $path_files[] = $dir."/erroneos/".$file_name;
                 }//end if
 
+                //Error 2 [Falta información en requerida en el contenido del excel]
                 if($err == 2){
                     rename($dir."/".$file_name, $dir."/erroneos/".$file_name);
-                    echo decodeError($err,$arr_error,$file_name);
+                    $decode_error .= decodeError($err,$arr_error,$file_name);
+                    $path_files[] = $dir."/erroneos/".$file_name;
+                }//end if
+
+                //Error 404 [No se encontro el archivo]
+                if($err == 404){
+                    $decode_error .= decodeError($err,$arr_error,$file_name);
                 }//end if
             }//end foreach
         }//end if
 
-
+        //Manda notifiación vía Email
+        $response_email = sendEmail($decode_error,$config,$path_files);
 
     }elseif(empty($files_xlsx) && empty($files_xls)){
-        echo "<h2>Sin archivo para procesar</h2>";
-        die();
+        die("<h2>Sin archivo para procesar</h2>");
     }//end if
 }//end if
 
@@ -167,18 +201,20 @@ function validaArchivo($file_name){
             return $arr_error_c;
         }//end if
     }else{
-        echo "No se puede encontrar el archivo especificado";
-        die();
+        $arr_error_c = ["err"=>"404","arr_error"=>null,"file_name"=>$file_name];
+        return $arr_error_c;
     }//end if
 }//end function
 
 function decodeError($err,$arr_error,$file_name){
+    $body = "";
     if($err == 0){
-        return "<h3> - El archivo : <strong style='color: green;'>".$file_name."</strong> es válido.</h3> <hr>";
+        $body .= "<h3> - El archivo : <strong style='color: green;'>".$file_name."</strong> es válido.</h3> <hr>";
+        return $body;
     }//end if
 
     if($err == 1){
-        $body = "<h3> - El archivo : <strong style='color: green;'>".$file_name."</strong> <strong style='color: red;'> NO </strong> es válido, ";
+        $body .= "<h3> - El archivo : <strong style='color: green;'>".$file_name."</strong> <strong style='color: red;'> NO </strong> es válido, ";
         $body .= "<span> faltan algunas columnas requeridas en el excel que a continuación se listan :</span> </h3>";
         $body .=  "<ul>";
         foreach ($arr_error as $value) {
@@ -191,7 +227,7 @@ function decodeError($err,$arr_error,$file_name){
     }//end if
 
     if($err == 2){
-        $body = "<h3> - El archivo : <strong style='color: green;'>".$file_name."</strong> <strong style='color: red;'> NO </strong> es válido, ";
+        $body .= "<h3> - El archivo : <strong style='color: green;'>".$file_name."</strong> <strong style='color: red;'> NO </strong> es válido, ";
         $body .= "falta información requerida en el archivo, a continuación se listan columnas y filas con información faltante : </h3>";
 
         $body .= "
@@ -214,5 +250,43 @@ function decodeError($err,$arr_error,$file_name){
 
         return $body;
     }
+
+    if($err == 404){
+        $body .= "<h3> - El archivo con el nombre: <strong style='color: green;'>".$file_name."</strong> no se encontra en la ruta especificada.</h3> <hr>";
+        return $body;
+    }
+}//end function
+
+function sendEmail($HTML=null,$config=null,$path_files=null){
+    $phpmailer             = new PHPMailer();
+    $phpmailer->IsSMTP();
+    $phpmailer->SMTPAuth   = true;// enable SMTP authentication
+    $phpmailer->SMTPSecure = $config["smpt_secure"];
+    $phpmailer->Host       = $config["smpt_server"]; // sets the SMTP server
+    $phpmailer->Port       = $config["smpt_port_conection"];// set the SMTP port for the GMAIL server
+    $phpmailer->Username   = $config["smpt_user"]; // SMTP account username
+    $phpmailer->Password   = $config["smpt_pwd"];// SMTP account password
+    $phpmailer->SMTPDebug  = 0;
+    $phpmailer->From       = $config["smpt_user"] ;
+    $phpmailer->FromName   = utf8_decode($config["smpt_name_from"]);
+    $phpmailer->Subject    = utf8_decode($config["subject_title"]);
+    $arr_to = explode(",",$config["to"]);
+    foreach ($arr_to as $to_) {
+        $phpmailer->AddAddress($to_);
+    }//end foreach
+
+    //Attachments
+    if(!is_null($path_files)){
+        foreach ($path_files as $file) {
+            $phpmailer->addAttachment($file);
+        }
+    }//end if
+    $phpmailer->MsgHTML(utf8_decode($HTML));
+    if (!$phpmailer->send()) {
+        echo 'Mailer Error: ' . $phpmailer->ErrorInfo;
+    } else {
+        echo 'Report Message sent!';
+    }//end if
+    die();
 }//end function
 ?>
